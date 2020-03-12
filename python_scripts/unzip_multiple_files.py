21061a92cf27570fce936355bab6d6631e295bc1
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-03-12 11:13:55 +0530

    Add unzip multiple files

---
+++ python_scripts/unzip_multiple_files.py
@@ -0,0 +1,24 @@
+'''
+This script will unzip multiple zip files places at different sub-directories of same parent directory.
+'''
+
+import os
+import zipfile
+import sys
+
+def unzip_files():
+    DIR=sys.argv[1]
+
+    for (root, dir, files) in os.walk(DIR):
+        for fname in files:
+            print(fname)
+            if('.zip' in fname):
+                zip_file = os.path.join(root, fname)
+                zipref = zipfile.ZipFile(zip_file, 'r')
+                zipref.extractall(root)
+                zipref.close()
+                print(zip_file, 'is extracted Successfully!')
+
+
+if __name__ == "__main__":
+    unzip_files()
